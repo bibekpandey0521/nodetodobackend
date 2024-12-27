cc00ac16b8ab10bfaf964de689d469cff18449de
bibekpandey0521 <bibekpandey276@gmail.com>
2024-12-27 15:43:41 +0545

    Todo Assessement

---
+++ index.js
@@ -0,0 +1,63 @@
+import express  from "express";
+import mongoose from "mongoose";
+import dotnev from 'dotenv';
+import { Todo } from "./model.js"
+
+
+const app = express();
+app.use(express.json());
+dotnev.config();
+const port = 8000;
+
+async function connectDB() {
+    const connection = await mongoose.connect(process.env.MONGO_URL);
+    console.log(connection.connection.host);
+
+}
+connectDB();
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+app.get('/', async (req, res) => {
+    const user = await Todo.find();
+    res.status(200).json({
+        user,
+    });
+});
+app.post('/',async(req,res) => {
+    const newTodo = req.body;
+    const todo = await Todo.create(newTodo);
+    res.status(201).json({todo});
+});
+
+app.put('/:id',async(req,res)=>{
+    const {id} = req.params;
+    const data = req.body;
+    const user = await Todo.findById(id);
+    if(user){
+        const updatedTodo = await Todo.updateOne({
+         _id:id,   
+        },
+        data
+    );
+    return res.status(200).json({ updatedTodo });
+
+    }
+    return res.status(404).json({ message: 'User doesnot exist' });
+
+});
+
+app.delete('/:id',async(req,res)=>{
+    const {id} = req.params;
+    console.log(req.params.id)
+    const user = await Todo.findById(id);
+    if(user){
+        const deleteTodo = await Todo.deleteOne({
+            _id:id,
+        });
+        return res.status(200).json({deleteTodo});
+    }
+    return res.status(404).json({
+        message:'User doesnot exits'
+    });
+});
